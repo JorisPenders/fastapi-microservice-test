02048c21f7a9ecf90bccc9f4b48f35ceb239e2d3
Joris Penders <joris.penders@rabobank.nl>
2023-02-23 17:20:07 +0100

    adding post data to call

--- service3/app/main.py
+++ service3/app/main.py
@@ -16,17 +16,20 @@ s2 = requests.Session()
 # establish connection
 s2.get('https://gateway-nginx:81', verify=False)
 
+size = 512 * 1024 * 2 # Size in bytes
+byte_string = bytes(bytearray([0] * size))
+
 
 @app.get("/session")
 def read_root():
     start = time.time()
 
-    service_1_req = s1.get('https://gateway-nginx:81', verify=False).json()
+    service_1_req = s1.post('https://gateway-nginx:81', data=byte_string, verify=False).json()
 
     # simulate network overhead
     # time.sleep(60 / 1000)
 
-    service_2_req = s2.get('https://gateway-nginx:82', verify=False).json()
+    service_2_req = s2.post('https://gateway-nginx:82', data=byte_string, verify=False).json()
 
     # simulate network overhead
     # time.sleep(60 / 1000)
@@ -35,7 +38,7 @@ def read_root():
     total_time = (end - start)
 
     return {
-        'elapsed time': total_time,
+        'elapsed time in ms': total_time * 1000,
         'json_first': service_1_req,
         'json_second': service_2_req
     }
