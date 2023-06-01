e57d05c4979bc4a256eb6b4f640fdb5e2c50cab5
jorispenders@gmail.com <joris.penders@rabobank.nl>
2023-06-01 21:35:24 +0200

    testing with postgres

--- service1/app/main.py
+++ service1/app/main.py
@@ -3,15 +3,50 @@ from typing import Union
 from fastapi import FastAPI
 from random import randint
 from time import sleep
+import time
 
 
 app = FastAPI()
 
+import psycopg2
 
+# Establishing a connection to the PostgreSQL database
+conn = psycopg2.connect(
+    host="192.168.68.114",
+    database="sportsdb",
+    user="postgres",
+    password="postgres"
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/db")
+def get_db():
+    st = time.time()
+
+    # Creating a cursor object
+    cur = conn.cursor()
+
+    # Executing a SELECT query
+    cur.execute("SELECT * FROM public.baseball_event_states LIMIT 1")
+
+    # Fetching all rows from the result set
+    rows = cur.fetchall()
+
+    # Closing the cursor and connection
+    cur.close()
+
+    et = time.time()
+
+    print(f"Succesful request to the database: {rows}")
+    print(f"Call took: {(et - st) * 1000:.2f} ms")
+
+    return {
+        'results': rows,
+        'query_time_to_db': (et - st) * 1000
+    }
 
 @app.post("/")
 def read_root_2():
--- service3/app/main.py
+++ service3/app/main.py
@@ -59,7 +59,7 @@ trx_sample = {
   "routing_number": "123456789",
   "batch_id": "BATCH202305"
 }
-set_of_trx = [trx_sample] * 25
+set_of_trx = [trx_sample] * 150
 
 
 @app.get("/session")
@@ -67,18 +67,17 @@ def read_root():
     start = time.time()
 
     # loading stuff
-    time.sleep(150 / 1000)
+    # time.sleep(150 / 1000)
 
-
-    service_1_req = s1.post('https://gateway-nginx:81', json=set_of_trx, verify=False).json()
+    service_1_req = s1.post('https://gateway-nginx:81/db', json=set_of_trx, verify=False).json()
 
     # simulate network overhead
-    time.sleep(60 / 1000)
+    # time.sleep(60 / 1000)
 
     service_2_req = s2.post('https://gateway-nginx:82', json=set_of_trx, verify=False).json()
 
     # simulate network overhead
-    time.sleep(60 / 1000)
+    # time.sleep(60 / 1000)
 
     end = time.time()
     total_time = (end - start)
